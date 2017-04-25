7db2e0100204e28deb846411d213e549d67cda15
Mike Curtin <michael_j_curtin@yahoo.com>
2017-04-24 21:55:32 -0400

    Added login/out functionality

--- roomtemp/settings.py
+++ roomtemp/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'roomtemp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['rtemp/templates/rtemp',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,3 +125,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/'
--- rtemp/urls.py
+++ rtemp/urls.py
@@ -1,6 +1,8 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url('^accounts/', include('django.contrib.auth.urls')),
 ]
